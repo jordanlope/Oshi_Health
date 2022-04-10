da7bdc3daa3be9675ab57c50bbb4394b421b4cac
Jordan Lopez <jordanlopez992@gmail.com>
2022-04-10 04:12:23 -0400

    Established navbar with camera button

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AllSelfies from './Screens/AllSelfies';
 import AddSelfie from './Screens/AddSelfie';
+import IconButton from './Components/UI/IconButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,19 @@ export default function App() {
       <StatusBar style='dark' />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Selfies" component={AllSelfies} />
+          <Stack.Screen 
+            name="Selfies" 
+            component={AllSelfies} 
+            options={({ navigation }) => ({
+              headerRight: ({tintColor}) => (
+                <IconButton 
+                  icon="camera" 
+                  size={24} 
+                  color={tintColor} 
+                  onPress={() => navigation.navigate('Selfie')}/>
+              )
+            })} 
+          />
           <Stack.Screen name="Selfie" component={AddSelfie} />
         </Stack.Navigator> 
       </NavigationContainer>
