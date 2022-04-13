bcaade2e27349f42d1b835ccd04760c8354b4338
Jordan Lopez <jordanlopez992@gmail.com>
2022-04-13 01:47:27 -0400

    Re-established the Camera feature with expo-image-picker

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AllSelfies from './Screens/AllSelfies';
-import AddSelfie from './Screens/AddSelfie';
 import IconButton from './Components/UI/IconButton';
-import Camera from './Screens/Camera';
-import takeImageHandler from './API/takeImageHandler';
+import ImagePicker from './Components/ImagePicker';
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
+
+  async function verifyPermission() {
+    if(cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+  
+      return permissionResponse.granted;
+    }
+  
+    if(cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert(
+        "Insufficent Permissions", 
+        "Please grant camera permissions to use this app");
+      return false;
+    }
+  
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermission();
+    if(!hasPermission) {
+      return;
+    }
+    const image = await launchCameraAsync({
+        allowsEditing: false,
+        aspect: [16, 9],
+        quality: 0.5,
+        cameraType: 'front',
+        mediaType: 'photo'
+    });
+
+    console.log(image);
+}
+
   return (
     <>
       <StatusBar style='dark' />
@@ -26,16 +59,15 @@ export default function App() {
                   icon="camera" 
                   size={24} 
                   color={tintColor} 
-                  onPress={() => {
-                    navigation.navigate('Selfie');
-                  }}/> //This should change to a screen
+                  onPress={takeImageHandler}
+                /> //This should change to a screen
               )
             })}
           />
           <Stack.Screen 
             name="Selfie" 
-            component={Camera} 
-            options={() => ({
+            component={ImagePicker} 
+            options={() => ({ // For example only
               title: "Selfie"
             })}/>
         </Stack.Navigator> 
