4284fd02f66436af0f7c7eb7d57e81cc02966f88
Jordan Lopez <jordanlopez992@gmail.com>
2022-04-14 22:44:32 -0400

    Adjustments to Date being displayed

--- models/selfie.js
+++ models/selfie.js
@@ -2,20 +2,21 @@ export class Selfie {
     constructor(imageUri) {
         this.imageUri = imageUri;
         this.year = new Date().getFullYear();
-        this.month = new Date().getMonth();
-        this.day = new Date().getDay();
+        this.month = new Date().getMonth() + 1;
+        this.day = new Date().getDate();
         this.hour = new Date().getHours();
         this.minutes = new Date().getMinutes();
         this.id = new Date().toString() + Math.random().toString();
     }
 
     getTime() {
-        const time = (this.hour > 12 ? `${(this.hour-12).toString()}:${this.minutes}pm` : `${(this.hour).toString()}:${this.minutes}am`);
+        const minutes = (this.minutes < 10 ? `0${this.minutes}` : `${this.minutes}`);
+        const time = (this.hour > 12 ? `${(this.hour-12).toString()}:${minutes}pm` : `${(this.hour).toString()}:${minutes}am`);
         return time;
     }
 
     getDate() {
-        const date = `${this.day}/${this.month}/${this.year}`
+        const date = `${this.month}/${this.day}/${this.year}`
         return date;
     }
 }
